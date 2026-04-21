039acdf365e24f1ca846b5cf25262fb1392b8186
Matheus Martins <math.fmartins42@gmail.com>
2026-04-21 18:04:18 -0300

    playing with modules, import and export

---
+++ modules/main.js
@@ -0,0 +1,5 @@
+import xpto from './math.js';
+console.log(xpto.sum(3, 7));
+console.log(xpto.multiply(3, 7));
+console.log(xpto.divide(10, 2));
+console.log(xpto.sub(10, 4));
---
+++ modules/math.js
@@ -0,0 +1,11 @@
+import { sum } from './sum.js';
+import { multiply }  from './multiply.js';
+import { sub } from './sub.js';
+import { divide } from './divide.js';
+
+export default {
+    sum,
+    multiply,
+    sub,
+    divide
+}
