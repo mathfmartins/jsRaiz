7fa4c2af49fb1d8b0c2a7415bc913f80d77afd69
Matheus Martins <math.fmartins42@gmail.com>
2026-05-06 15:44:50 -0300

    refactor: change import name

--- modules/main.js
+++ modules/main.js
@@ -1,5 +1,5 @@
-import xpto from './math.js';
-console.log(xpto.sum(3, 7));
-console.log(xpto.multiply(3, 7));
-console.log(xpto.divide(10, 2));
-console.log(xpto.sub(10, 4));
+import math from './math.js';
+console.log(math.sum(3, 7));
+console.log(math.multiply(3, 7));
+console.log(math.divide(10, 2));
+console.log(math.sub(10, 4));
